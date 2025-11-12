cf9e0a41cfc3708ee42f81a7bf678775c8847666
Md. Al Assadulla <assad.it2003@gmail.com>
2025-11-13 00:23:01 +0600

    11

---
+++ android/app/src/main/kotlin/com/assad/app2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.assad.app2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,287 @@
+   
+import 'package:flutter/material.dart';
+
+void main (){
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color(0xffF1F8FD),
+        appBar: AppBar(
+          backgroundColor: Color(0xffF1F8FD),
+          leading: Icon(Icons.arrow_back),
+centerTitle: true,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage("Train.jpg"),
+
+              ),
+              Text("care")
+            ],
+
+          ),
+          actions: [
+            Icon(Icons.tune),
+            SizedBox(width: 15,)
+          ],
+        ),
+
+body: Column(
+  children: [
+    Container(
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.symmetric(horizontal: 10),
+      height: 40,
+      width: double.infinity,
+      decoration:BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15),
+      ) ,
+      child: Row(
+        children: [
+          Icon(Icons.arrow_back),Text("Search")
+        ],
+      ),
+    ),
+SingleChildScrollView(
+  scrollDirection: Axis.horizontal,
+
+  child:
+  Row(
+    spacing: 10,
+    children: [
+      Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+
+        ),
+        child: Text("baby & care"),
+      ),
+      Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+
+        ),
+        child: Text("baby & care"),
+      ),
+      Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+
+        ),
+        child: Text("baby & care"),
+      ),
+      Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+
+        ),
+        child: Text("baby & care"),
+      ),
+      Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+
+        ),
+        child: Text("baby & care"),
+      ),
+      Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+
+        ),
+        child: Text("baby & care"),
+      ),
+      Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+
+        ),
+        child: Text("baby & care"),
+      ),
+      Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+
+        ),
+        child: Text("baby & care"),
+      ),
+      Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+
+        ),
+        child: Text("baby & care"),
+      ),
+      Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+
+        ),
+        child: Text("baby & care"),
+      ),
+      Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+
+        ),
+        child: Text("baby & care"),
+      ),
+      Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+
+        ),
+        child: Text("baby & care"),
+      ),
+      Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+
+        ),
+        child: Text("baby & care"),
+      ),
+      Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+
+        ),
+        child: Text("baby & care"),
+      ),
+      Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+
+        ),
+        child: Text("baby & care"),
+      ),
+      Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+
+        ),
+        child: Text("baby & care"),
+      ),
+      Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+
+        ),
+        child: Text("baby & care"),
+      ),
+      Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+
+        ),
+        child: Text("baby & care"),
+      ),
+      Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+
+        ),
+        child: Text("baby & care"),
+      ),
+      Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+
+        ),
+        child: Text("baby & care"),
+      ),
+      Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+
+        ),
+        child: Text("baby & care"),
+      ),
+      Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+
+        ),
+        child: Text("baby & care"),
+      ),
+      Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+
+        ),
+        child: Text("baby & care"),
+      ),
+      Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+
+        ),
+        child: Text("baby & care"),
+      ),
+
+    ],
+  ),
+),
+
+
+
+          
+        ],
+      ),
+    )
+
+),
+
+
+  );
+}
